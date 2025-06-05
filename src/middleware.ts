261b4a6ea7c1ebb34553600616aa6c55c5192491
Bruno <brunoguilher1@gmail.com>
2025-06-05 18:12:12 -0300

    feat: Implement middleware for route authentication and redirection

---
+++ src/middleware.ts
@@ -0,0 +1,41 @@
+import { MiddlewareConfig, NextRequest, NextResponse } from "next/server"
+
+const publicRoutes = [
+  { path: '/', whenAuthenticated: 'redirect' },
+  { path: '/dashboard/eventos', whenAuthenticated: 'next' },
+] as const
+
+const REDIRECT_WHEN_NOT_AUTENTICATED_ROUTE = '/'
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const publicRoute = publicRoutes.find(route => route.path === path);
+  const authToken = request.cookies.get('token');
+
+  if (!authToken && publicRoute) {
+    return NextResponse.next();
+  }
+
+  if (!authToken && !publicRoute) {
+    const redirectUrl = request.nextUrl.clone();
+    redirectUrl.pathname = REDIRECT_WHEN_NOT_AUTENTICATED_ROUTE;
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  if (authToken && publicRoute && publicRoute.whenAuthenticated === 'redirect') {
+    return NextResponse.next();
+  }
+}
+
+export const config: MiddlewareConfig = {
+    matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next|.*\\..*).*)',
+  ],
+}
