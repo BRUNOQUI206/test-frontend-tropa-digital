7498a3b8f0fef7827089ae4ca500ead61f2ecd21
Bruno <brunoguilher1@gmail.com>
2025-06-06 20:12:53 -0300

    fix: Adjust button component padding for improved layout

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,7 +13,7 @@ export const Button = ({ children, disabled, type = 'submit', onClick }: ButtonP
       type={type}
       onClick={onClick}
       disabled={disabled}
-      className="w-full rounded-full bg-orange-700/80 py-2 text-sm font-medium text-white hover:bg-orange-700/80 disabled:cursor-not-allowed disabled:bg-orange-700/80 disabled:opacity-50"
+      className="rounded-full bg-orange-700/80 py-2 px-4 text-sm font-medium text-white hover:bg-orange-700/80 disabled:cursor-not-allowed disabled:bg-orange-700/80 disabled:opacity-50"
     >
       {children}
     </button>
